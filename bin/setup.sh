7c16f0335e63d5b82aa6b1b13fcdd5e665e41aef
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2026-02-24 11:15:21 +0530

    Moves files with force overwrite option

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,5 +2,5 @@ curl -L https://github.com/PrasadThatipudi/central-spec-configs/archive/refs/hea
 unzip -o main.zip
 rm main.zip
 
-mv central-spec-configs-main/.github .
-mv central-spec-configs-main/.specify .
+mv -f central-spec-configs-main/.github .
+mv -f central-spec-configs-main/.specify .
