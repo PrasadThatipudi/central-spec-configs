20c02996a5945f17400230ab34b7c5a1c245c1f1
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2026-02-24 11:01:52 +0530

    Extracts repo structure from GitHub

---
+++ bin/setup.sh
@@ -0,0 +1,3 @@
+curl -L https://github.com/PrasadThatipudi/central-spec-configs/archive/refs/heads/main.zip -o main.zip
+unzip -o main.zip
+rm main.zip
--- bin/speckit.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env deno
-
-console.log("Command line arguments: ", Deno.args);
-console.log("Current directory: ", Deno.cwd());
-console.log(
-  "File content: \n",
-  Deno.readTextFileSync("./.github/agents/speckit.analyze.agent.md"),
-);
