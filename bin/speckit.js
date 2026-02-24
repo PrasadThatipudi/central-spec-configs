1cc0ed9310843e2a7f60b9883582486369f72438
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2026-02-24 10:48:03 +0530

    Reads agent file

--- bin/speckit.js
+++ bin/speckit.js
@@ -2,3 +2,7 @@
 
 console.log("Command line arguments: ", Deno.args);
 console.log("Current directory: ", Deno.cwd());
+console.log(
+  "File content: \n",
+  Deno.readTextFileSync("./.github/agents/speckit.analyze.agent.md"),
+);
