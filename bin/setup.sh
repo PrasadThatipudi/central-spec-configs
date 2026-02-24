4595d475a6d260aa9564c6a1e9d61cfe02d25243
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2026-02-24 11:06:14 +0530

    Moves .github and .specify folders to current directory

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,3 +1,6 @@
 curl -L https://github.com/PrasadThatipudi/central-spec-configs/archive/refs/heads/main.zip -o main.zip
 unzip -o main.zip
 rm main.zip
+
+mv central-spec-configs-main/.github .
+mv central-spec-configs-main/.specify .
